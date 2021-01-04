439d62b7854d034409f081b1ed4723b1932afd33
hatem20 <hatem.said50@gmail.com>
2021-01-05 01:28:10 +0200

    move start dir outside and remove other files

---
+++ assets/app.js
@@ -0,0 +1,24 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+// any CSS you import will output into a single css file (app.css in this case)
+import './styles/app.css';
+import getNiceMsg from './js/get_nice_message';
+import $ from 'jquery';
+// global.$ = $;
+// start the Stimulus application
+import 'bootstrap';
+
+console.log(getNiceMsg(4));
+
+$('.dropdown-toggle').dropdown();
+$('.custom-file-input').on('change', function(event) {
+    var inputFile = event.currentTarget;
+    $(inputFile).parent()
+        .find('.custom-file-label')
+        .html(inputFile.files[0].name);
+});
